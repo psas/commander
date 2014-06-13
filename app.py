6253c9f3531f26c1adeff7770a4344ed821e75e8
Nathan Bergey <nathan.bergey@gmail.com>
2014-06-13 01:32:29 -0700

    some kind of flask app

---
+++ app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from flask import Flask, render_template
+import yaml
+import json
+import os
+import glob
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Find all .yml files
+    files_yml = glob.glob(os.path.join(os.path.dirname(os.path.realpath(__file__)), "profiles/*.yml"))
+
+    # Make lise of files + names
+    Profiles = []
+    for f in files_yml:
+        with open(f, 'r') as y:
+            p = yaml.load(y, Loader=yaml.Loader)
+            Profiles.append({
+                'name': p['title'],
+                'slug': p['title'],
+                'uri': "/profiles/"+p['title'],
+                'file': f
+            })
+
+    return render_template('index.html', layouts=Profiles)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
