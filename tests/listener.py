b2314a1b138340151854a7604279afd3c795bb7d
Nathan Bergey <nathan.bergey@gmail.com>
2014-06-27 13:08:39 -0700

    test listener app running in background

--- tests/listener.py
+++ tests/listener.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 import socket
+import time
 
 HOST = ''                 # Symbolic name meaning the local host
 PORT = 2223              # Arbitrary non-privileged port
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
-
-
 s.listen(1)
-conn, addr = s.accept()
-print 'Connected by', addr
+
 while 1:
-    data = conn.recv(1024)
-    if not data: continue
-    print data
-    conn.send("Unknown")
-conn.close()
+    conn, addr = s.accept()
+    print 'Connected by', addr
+    while 1:
+        data = conn.recv(1024)
+        if not data: continue
+        print data
+        conn.send("Unknown")
+        conn.close()
+        break
