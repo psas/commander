da0665ca2f6c97bee1e443a397a0dd17aa49f38c
Nathan Bergey <nathan.bergey@gmail.com>
2015-06-21 10:53:08 -0700

    fake RNH in test listener

--- tests/listener.py
+++ tests/listener.py
@@ -1,22 +1,35 @@
 #!/usr/bin/env python
 import socket
 import time
+from contextlib import closing
+
 
 HOST = ''                 # Symbolic name meaning the local host
 PORT = 2223              # Arbitrary non-privileged port
 
+ports = 0
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
+with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
 
-while 1:
-    conn, addr = s.accept()
-    print 'Connected by', addr
     while 1:
-        data = conn.recv(1024)
-        if not data: continue
-        print data
-        conn.send("data from device")
-        conn.close()
-        break
+        conn, addr = s.accept()
+        while 1:
+            data = conn.recv(1024)
+            if not data: continue
+            print data
+
+
+            if 'OFF' in data:
+                ports = 127
+            elif 'XFF' in data:
+                ports = 0
+            elif data[5] == 'O':
+                ports |= int(data[6:])
+            elif data[5] == 'X':
+                ports &= ~int(data[6:])
+
+            conn.send(str(ports))
+            conn.close()
+            break
